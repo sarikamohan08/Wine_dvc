86c7010f0bfc3421f065bfcda7557cc214ea7e51
sarikamohan08 <sarikamohan08@gmail.com>
2022-02-06 11:38:58 +0530

    stage 01 utils added

---
+++ src/utils/all_utils.py
@@ -0,0 +1,15 @@
+import yaml
+import os
+
+import yaml
+import os
+import json
+
+
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+
+    return content
+
+
